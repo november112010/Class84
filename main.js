c5fcdb567b898c864c1f29abe8c714624faefd7f
november112010 <84222404+november112010@users.noreply.github.com>
2021-07-03 13:10:21 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,16 +1,33 @@
 canvas=document.getElementById("myCanvas");
 ctx=canvas.getContext("2d");
+rover_width=100;
+rover_height=90;
+background_image="mars.jpg";
+rover_image="rover.png";
+rover_x=10;
+rover_y=10;
 function add(){
-background_imgTag=new Image();
-background_imgTag.src=}
+ background_imgTag=new Image();
+ background_imgTag.onload=uploadBackground;
+ background_imgTag.src=background_image;
+ 
+ rover_imgTag=new Image();
+ rover_imgTag.onload=uploadrover;
+ rover_imgTag.src=rover_image;
+}
+function uploadBackground(){
+ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);    
+}
+function uploadrover(){
+ctx.drawImage(rover_imgTag,rover_x,rover_y,rover_width,rover_height);    
+}
 window.addEventListener("keydown",my_Keydown);
 function my_Keydown(e){
 KeyPressed=e.keyCode;
 console.log(KeyPressed);
-if((KeyPressed >= '97' && KeyPressed <='122')|| (KeyPressed >='65' && KeyPressed<=90)){
-alphabetkeys();
-document.getElementById("d1").innerHTML="You pressed the Alphabet key"
-console.log("Alphabets");    
+if(KeyPressed == '38'){
+up();
+console.log("up");    
 }
  if(KeyPressed == '40'){
  down();
@@ -25,3 +42,35 @@ console.log("Alphabets");
  console.log("right");    
  }    
 }
+function up(){
+if(rover_y >= 0){
+rover_y = rover_y-10;
+console.log("When up arrow is pressed, x= "+rover_x+"|y= "+rover_y);
+uploadBackground();
+uploadrover();    
+}    
+}
+function down(){
+    if(rover_y <= 500){
+    rover_y = rover_y+10;
+    console.log("When down arrow is pressed, x= "+rover_x+"|y= "+rover_y);
+    uploadBackground();
+    uploadrover();    
+    }    
+    }
+    function left(){
+        if(rover_x >= 0){
+        rover_x = rover_x-10;
+        console.log("When left arrow is pressed, x= "+rover_x+"|y= "+rover_y);
+        uploadBackground();
+        uploadrover();    
+        }    
+        }
+    function right(){
+            if(rover_x <= 700){
+            rover_x = rover_x+10;
+            console.log("When right arrow is pressed, x= "+rover_x+"|y= "+rover_y);
+            uploadBackground();
+            uploadrover();    
+            }    
+            }
