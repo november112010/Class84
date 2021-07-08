20fb6550d0045a5b3ed75c546b1cf2795aba2f67
november112010 <84222404+november112010@users.noreply.github.com>
2021-07-08 14:01:17 +0600

    Add files via upload

--- main.js
+++ main.js
@@ -1,76 +1,85 @@
-canvas=document.getElementById("myCanvas");
-ctx=canvas.getContext("2d");
-rover_width=100;
-rover_height=90;
-background_image="mars.jpg";
-rover_image="rover.png";
-rover_x=10;
-rover_y=10;
-function add(){
- background_imgTag=new Image();
- background_imgTag.onload=uploadBackground;
- background_imgTag.src=background_image;
- 
- rover_imgTag=new Image();
- rover_imgTag.onload=uploadrover;
- rover_imgTag.src=rover_image;
+canvas = document.getElementById('myCanvas');
+ctx = canvas.getContext("2d");
+
+img_width = 300;
+img_height = 100;
+
+var img_image;
+
+img_x = 100;
+img_y = 100;
+
+function add() {
+	img_imgTag = new Image(); //defining a variable with a new image
+	img_imgTag.onload = uploading; // setting a function, onloading this variable
+	img_imgTag.src = img_image;   // load image
 }
-function uploadBackground(){
-ctx.drawImage(background_imgTag,0,0,canvas.width,canvas.height);    
+
+function uploading() {
+
+	ctx.drawImage(img_imgTag, img_x, img_y, img_width, img_height);
 }
-function uploadrover(){
-ctx.drawImage(rover_imgTag,rover_x,rover_y,rover_width,rover_height);    
+
+window.addEventListener("keydown", my_keydown);
+
+function my_keydown(e)
+{
+	keyPressed = e.keyCode;
+	console.log(keyPressed);
+	
+		if((keyPressed >=97 && keyPressed<=122)|| (keyPressed >=65 && keyPressed<=90))
+		{
+			aplhabetkey();
+			document.getElementById("d1").innerHTML="You pressed Alphabet Key";
+			console.log("alphabet key");
+		}
+		else if(keyPressed >=48 && keyPressed<=57)
+		{
+			numberkey();
+			document.getElementById("d1").innerHTML="You pressed Number Key";
+			console.log("Number key");
+		}
+		else if(keyPressed >=37 && keyPressed<=40)
+		{
+			arrowkey();
+			document.getElementById("d1").innerHTML="You pressed Arrow Key";
+			console.log("Arrow Key");
+		}
+		else if((keyPressed ==17)|| (keyPressed ==18 || keyPressed ==27))
+		{
+			specialkey();
+			document.getElementById("d1").innerHTML="You pressed ctrl/esc/alt";
+			console.log("special key");
+		}
+	else{
+		otherkey();
+		document.getElementById("d1").innerHTML="You pressed symbol or other key";
+	}
 }
-window.addEventListener("keydown",my_Keydown);
-function my_Keydown(e){
-KeyPressed=e.keyCode;
-console.log(KeyPressed);
-if(KeyPressed == '38'){
-up();
-console.log("up");    
+
+function aplhabetkey()
+{
+		img_image="Alpkey.png";
+	add();
 }
- if(KeyPressed == '40'){
- down();
- console.log("down")    
- }
- if(KeyPressed == '37'){
- left();
- console.log("left");    
- }
- if(KeyPressed == '39'){
- right();
- console.log("right");    
- }    
+function numberkey()
+{
+	img_image="numkey.png";
+	add();
 }
-function up(){
-if(rover_y >= 0){
-rover_y = rover_y-10;
-console.log("When up arrow is pressed, x= "+rover_x+"|y= "+rover_y);
-uploadBackground();
-uploadrover();    
-}    
+function arrowkey()
+{
+	img_image="Arrkey.png";
+	add();
 }
-function down(){
-    if(rover_y <= 500){
-    rover_y = rover_y+10;
-    console.log("When down arrow is pressed, x= "+rover_x+"|y= "+rover_y);
-    uploadBackground();
-    uploadrover();    
-    }    
-    }
-    function left(){
-        if(rover_x >= 0){
-        rover_x = rover_x-10;
-        console.log("When left arrow is pressed, x= "+rover_x+"|y= "+rover_y);
-        uploadBackground();
-        uploadrover();    
-        }    
-        }
-    function right(){
-            if(rover_x <= 700){
-            rover_x = rover_x+10;
-            console.log("When right arrow is pressed, x= "+rover_x+"|y= "+rover_y);
-            uploadBackground();
-            uploadrover();    
-            }    
-            }
+function specialkey()
+{
+	img_image="spkey.png";
+	add();
+}
+function otherkey()
+{
+	img_image="otherkey.png";
+	add();
+}
+	
